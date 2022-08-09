bce7f4fff5cbffd89fa810c314fc2a4cde0feb3e
William Justin <williamjustinxu@gmail.com>
2022-08-09 10:24:59 +0700

    add shorthand and fastio

--- template.cpp
+++ template.cpp
@@ -1,5 +1,16 @@
 #include<bits/stdc++.h>
 
+#define ll long long
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+
 using namespace std;
 
 void solve(){
@@ -7,10 +18,14 @@ void solve(){
 }
 
 int main(){
+
+    fastio();
+
     int T=1;
     // cin>>T;
 
     for(int TT=0;TT<T;TT++){
+		// cout<<"Case "<<TT+1<<":\n";solve();
         solve();
     }
 
