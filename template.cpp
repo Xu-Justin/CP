ff8f67495c47be733d0b7b96effee199dd199c07
William Justin <william.justin@traveloka.com>
2025-10-03 17:26:09 +0700

    format template

--- template.cpp
+++ template.cpp
@@ -1,33 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 #define ll long long
 #define fi first
 #define se second
 #define mp make_pair
 #define pb push_back
-#define pii pair<int,int>
-#define pll pair<long long,long long>
- 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL);
 #define endl "\n"
 
 using namespace std;
 
-void solve(){
-    
-}
+void solve() {}
 
-int main(){
+int main() {
 
-    fastio();
+  fastio();
 
-    int T=1;
-    // cin>>T;
+  int T = 1;
+  // cin>>T;
 
-    for(int TT=0;TT<T;TT++){
-        // cout<<"Case "<<TT+1<<":\n";solve();
-        solve();
-    }
+  for (int TT = 0; TT < T; TT++) {
+    // cout << "Case " << TT + 1 << ":\n"; solve();
+    solve();
+  }
 
-    return 0;
+  return 0;
 }
